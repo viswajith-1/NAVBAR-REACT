fe6a66373a2040d4fc0aed342b5526dea012a955
Viswajith SB <viswajithsaji@gmail.com>
2024-10-13 00:01:55 +0530

    final project

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 const Navbar = () => {
   const path = window.location.pathname;
   return (
     <>
-    <nav className="nav">
-      <Link to="/" className="site-title">
-        Navbar
-      </Link>
-      <ul>
-        <CustomLink to='/contactus'>Contact Us</CustomLink>
-        <CustomLink to='/aboutus'>About Us</CustomLink>
-      </ul>
-    </nav>
+      <nav className="nav">
+        <Link to="/" className="site-title">
+          Navbar
+        </Link>
+        <ul>
+          <CustomLink to="/contactus">Contact Us</CustomLink>
+          <CustomLink to="/aboutus">About Us</CustomLink>
+        </ul>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-const CustomLink = ({to,children, ...props}) => {
-  const path = window.location.pathname;
+const CustomLink = ({ to, children, ...props }) => {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
   return (
     <Link to={to} {...props}>
-          <li className={path===to?"active":""}>{children}</li>
+      <li className={isActive ? "active" : ""}>{children}</li>
     </Link>
-  )
-}
+  );
+};
 
 export default Navbar;
